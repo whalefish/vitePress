a33e75b091878680f94d239b8f245a6179af0f30
張珈語 <chiayu.chang@udngroup.com.tw>
2024-09-26 09:56:11 +0800

    調整 資料名稱

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,35 +19,38 @@ export default defineConfig({
             { text: '手冊', link: '/manuals/intro' },
         ],
 
-        // sidebar: [{
-        //     text: '手冊',
-        //     items: getFiles(path.resolve(__dirname, '../manuals/')),
-        // }],
+        sidebar: [
+            {
+                text: '手冊',
+                items: getFiles(path.resolve(__dirname, '../manuals/')),
+            }
+        ],
 
-        sidebar: generateSidebar([
-            {
-                documentRootPath: 'docs', // 設定你的文件根目錄為 /docs
-                scanStartPath: 'test01',   // 設定側邊欄只顯示 test01 資料夾的內容
-                resolvePath: '/test01/',
-                includeFolderIndexFile: true, // 包含該資料夾的 index.md 作為主要連結
-                useFolderTitleFromIndexFile: true,
-                // useTitleFromFrontmatter: true, // 啟用從 Frontmatter 提取標題
-                collapsed: false, // 預設展開該資料夾
-                collapseDepth: 2, // 控制展開的深度
-            },
-            {
-                documentRootPath: 'docs', // 設定你的文件根目錄為 /docs
-                scanStartPath: 'manuals',   // 設定側邊欄只顯示 manuals 資料夾的內容
-                resolvePath: '/manuals/',
-                includeFolderIndexFile: true,
-                collapsed: false,
-                useTitleFromFrontmatter: true,
-                useFolderTitleFromIndexFile: true, // 是否 讀取目前資料夾內index.md中的資料
-                useFolderLinkFromIndexFile: true, 
-                // sortMenusByFrontmatterOrder: true, // 利用order 排序
-                sortMenusOrderNumericallyFromTitle: true, // 使用title 排序
-            },
-        ]),
+        // sidebar: generateSidebar([
+        //     {
+        //         documentRootPath: 'docs', // 設定你的文件根目錄為 /docs
+        //         scanStartPath: 'test01',   // 設定側邊欄只顯示 test01 資料夾的內容
+        //         resolvePath: '/test01/',
+        //         includeFolderIndexFile: true, // 包含該資料夾的 index.md 作為主要連結
+        //         useFolderTitleFromIndexFile: true,
+        //         excludeFiles: ['index.md'], // 排除 index.md 文件
+        //         useTitleFromFrontmatter: true, // 啟用從 Frontmatter 提取標題
+        //         collapsed: false, // 預設展開該資料夾
+        //         // collapseDepth: 2, // 控制展開的深度
+        //     },
+        //     {
+        //         documentRootPath: 'docs', // 設定你的文件根目錄為 /docs
+        //         scanStartPath: 'manuals',   // 設定側邊欄只顯示 manuals 資料夾的內容
+        //         resolvePath: '/manuals/',
+        //         includeFolderIndexFile: true,
+        //         collapsed: false,
+        //         useTitleFromFrontmatter: true,
+        //         useFolderTitleFromIndexFile: true, // 是否 讀取目前資料夾內index.md中的資料
+        //         useFolderLinkFromIndexFile: true, 
+        //         // sortMenusByFrontmatterOrder: true, // 利用order 排序
+        //         sortMenusOrderNumericallyFromTitle: true, // 使用title 排序
+        //     },
+        // ]),
 
         socialLinks: [
             { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
